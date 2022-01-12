d63507ff1fa1e198f092dd643e9a368a601059ab
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-01-12 19:55:02 +0800

    完成 Envoy - Jaeger 示例

---
+++ src/EnvoyTrace/OrderService/PaymentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderService
+{
+    public class PaymentInfo
+    {
+        public string OrderId { get; set; }
+        public string PaymentId { get; set; }
+        public string Remark { get; set; }
+    }
+}
