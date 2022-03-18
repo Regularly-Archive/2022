f2a160db7336c0f71b3b275ffe02696737e0b06b
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-03-18 14:37:25 +0800

    实现分块上传下载

--- src/GrpcStream/Server/Services/FileService.cs
+++ src/GrpcStream/Server/Services/FileService.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf;
+using Grpc.Core;
 using SharedEntities;
 using System;
 using System.Collections.Generic;
@@ -10,18 +11,26 @@ namespace GrpcStreamServer.Services
 {
     public class FileService : SharedEntities.FileService.FileServiceBase
     {
+        private const int ChunkSize = 1024 * 512;
         public override async Task<UploadFileResponse> UploadFile(IAsyncStreamReader<UploadFileRequest> requestStream, ServerCallContext context)
         {
-            while (await requestStream.MoveNext())
+            var fileName = $"{Guid.NewGuid().ToString()}.png";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            using(var fileStream = File.Open(filePath, FileMode.Append, FileAccess.Write))
             {
-                var current = requestStream.Current;
-                var fileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(current.FileName)}";
-                var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-                await File.WriteAllBytesAsync(filePath, current.Content.ToByteArray());
+                var writeSize = 0;
+                while (await requestStream.MoveNext())
+                {
+                    var current = requestStream.Current;
+                    var bytes = current.ToByteArray();
+
+                    fileStream.Seek(writeSize, SeekOrigin.Begin);
+                    await fileStream.WriteAsync(bytes, 0, bytes.Length);
+                    writeSize += bytes.Length;
+                }
+
                 return new UploadFileResponse() { FilePath = fileName };
             }
-
-            return null;
         }
 
         public override async Task DownloadFile(DownloadFileRequest request, IServerStreamWriter<DownloadFileResponse> responseStream, ServerCallContext context)
@@ -29,10 +38,23 @@ namespace GrpcStreamServer.Services
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), request.FilePath);
             if (File.Exists(filePath))
             {
-                var fileBytes = File.ReadAllBytes(filePath);
-                var content = Google.Protobuf.ByteString.CopyFrom(fileBytes);
-                await responseStream.WriteAsync(new DownloadFileResponse() { Content = content });
+                using (var fileStream = File.OpenRead(filePath))
+                {
+
+                    var readSize = 0;
+                    var fileSize = fileStream.Length;
+
+                    var buffer = new byte[ChunkSize];
+                    while (readSize < fileSize)
+                    {
+                        fileStream.Seek(readSize, SeekOrigin.Begin);
+                        fileStream.Read(buffer, 0, buffer.Length);
+                        await responseStream.WriteAsync(new DownloadFileResponse() { Content = ByteString.CopyFrom(buffer), TotalSize = fileSize });
+                        readSize += buffer.Length;
+                    }
+                }
             }
+            
         }
     }
 }
