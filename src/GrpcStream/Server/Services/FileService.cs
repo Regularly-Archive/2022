d861818fd6fd9d4e6e073431fefb65b0e449b56f
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-03-18 13:35:49 +0800

    支持流式上传与下载

---
+++ src/GrpcStream/Server/Services/FileService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using SharedEntities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcStreamServer.Services
+{
+    public class FileService : SharedEntities.FileService.FileServiceBase
+    {
+        public override async Task<UploadFileResponse> UploadFile(IAsyncStreamReader<UploadFileRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var current = requestStream.Current;
+                var fileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(current.FileName)}";
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+                await File.WriteAllBytesAsync(filePath, current.Content.ToByteArray());
+                return new UploadFileResponse() { FilePath = fileName };
+            }
+
+            return null;
+        }
+
+        public override async Task DownloadFile(DownloadFileRequest request, IServerStreamWriter<DownloadFileResponse> responseStream, ServerCallContext context)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), request.FilePath);
+            if (File.Exists(filePath))
+            {
+                var fileBytes = File.ReadAllBytes(filePath);
+                var content = Google.Protobuf.ByteString.CopyFrom(fileBytes);
+                await responseStream.WriteAsync(new DownloadFileResponse() { Content = content });
+            }
+        }
+    }
+}
--- src/GrpcStream/Server/Startup.cs
+++ src/GrpcStream/Server/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GrpcStream.Services;
+using GrpcStreamServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,11 @@ namespace GrpcStream
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+            services.AddGrpc(options =>
+            {
+                options.MaxReceiveMessageSize = null; // 10 MB
+                options.MaxSendMessageSize = null; // 10 MB
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +38,7 @@ namespace GrpcStream
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<HeartBeatServicecs>();
+                endpoints.MapGrpcService<FileService>();
 
                 endpoints.MapGet("/", async context =>
                 {
