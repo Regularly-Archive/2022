965ac9b7da9d95728b2945c5bb97fb0c47885af2
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-04-18 16:06:45 +0800

    更新项目

--- src/EnvoyTrace/OrderService/Program.cs
+++ src/EnvoyTrace/OrderService/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace OrderService
 {
@@ -21,6 +22,9 @@ namespace OrderService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).UseSerilog((context, configure) =>
+                {
+                    configure.ReadFrom.Configuration(context.Configuration);
                 });
     }
 }
---
+++ src/front-mock/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MessageList msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MessageList from './components/MessageList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MessageList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
