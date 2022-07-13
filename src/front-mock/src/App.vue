cc29bfa5c95100ca8e4ffe6a917de371ab43e68f
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-07-13 16:15:41 +0800

    更新仓库

--- src/front-mock/src/App.vue
+++ src/front-mock/src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <MessageList msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <header>
+      <my-router-link to="/home">首页</my-router-link>
+      <span>/</span>
+      <my-router-link to="/message">消息</my-router-link>
+      <span>/</span>
+      <my-router-link to="https://blog.yuanpei.me" target="_blank"
+        >博客</my-router-link
+      >
+      <my-router-link
+        :to="{ path: 'https://www.baidu.com/s', query: { wd: '天气' } }"
+        target="_blank"
+        >百度</my-router-link
+      >
+      <span>/</span>
+      <my-router-link
+        :to="{
+          path: 'tencent://',
+          query: { uin: '875974254', site: 'Vue', menu: 'yes' },
+        }"
+        >QQ</my-router-link
+      >
+    </header>
+    <router-view />
   </div>
 </template>
 
 <script>
-import MessageList from './components/MessageList.vue'
+import MyRouterLink from "./components/MyRouterLink.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    MessageList
-  }
-}
+    MyRouterLink,
+  },
+};
 </script>
 
 <style>
