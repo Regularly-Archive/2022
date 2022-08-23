b762b1b6777eb2dc3b4ccda7705ff67bed1c9259
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-08-23 17:04:09 +0800

    增加 HelloWorld.Fody

---
+++ src/FodyWeavers/Program.cs
@@ -0,0 +1,60 @@
+﻿using FodyWeavers.Attributes;
+using HelloWorld.Fody;
+using JetBrains.Annotations;
+
+[assembly: PublicAPI]
+[module: PublicAPI]
+
+[UsedImplicitly]
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        //// Sync Method Logging
+        //Add(1, 2);
+
+        //// Async Method Logging 
+        //await AddAsync(1, 2);
+
+        //// Exception Handling
+        //Divide(1, 1);
+
+        //// NullGuard
+        //Split(null);
+
+        Echo();
+    }
+
+    [Logging]
+    static int Add(int a, int b) => a + b;
+
+    [Logging]
+    static Task<int> AddAsync(int a, int b) => Task.FromResult(a + b);
+
+    [Logging]
+    static decimal Divide(decimal a, decimal b) => a / b;
+
+    [NotNull]
+    public static string[] Split([NotNull][System.Diagnostics.CodeAnalysis.NotNull] Foo foo)
+    {
+        return foo.Colors.Split(new char[] { ',' });
+    }
+
+    [HelloWorld]
+    static void Echo()
+    {
+        Console.WriteLine("Hello Fody.");
+    }
+
+    [UsedImplicitly]
+    public class Foo
+    {
+        [NotNull]
+        public string Colors { get; set; }
+    }
+}
+
+
+
+
+
---
+++ src/HelloWorld.Fody/ModuleWeaver.cs
@@ -0,0 +1,64 @@
+﻿using Fody;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Fody
+{
+    public class ModuleWeaver : BaseModuleWeaver
+    {
+        private MethodInfo? _writeLineMethod  => typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) });
+        public override void Execute()
+        {
+            foreach (TypeDefinition type in ModuleDefinition.Types)
+            {
+                foreach (MethodDefinition method in type.Methods)
+                {
+                    var methodType = method.GetType();
+                    var customerAttributes = methodType.GetCustomAttributes(typeof(HelloWorldAttribute));
+                    if (customerAttributes != null && customerAttributes.Any())
+                    {
+                        ProcessMethod(method);
+                    }
+                }
+            }
+        }
+
+        public override IEnumerable<string> GetAssembliesForScanning()
+        {
+            yield return "mscorlib";
+            yield return "System";
+        }
+
+        private void ProcessMethod(MethodDefinition method)
+        {
+            // 获取当前方法体中的第一个 IL 指令
+            var processor = method.Body.GetILProcessor();
+            var current = method.Body.Instructions.First();
+
+            // 插入一个 Nop 指令，表示什么都不做
+            var first = Instruction.Create(OpCodes.Nop);
+            processor.InsertBefore(current, first);
+            current = first;
+
+            // 构造 Console.WriteLine("Hello World")
+            foreach (var instruction in GetInstructions(method))
+            {
+                processor.InsertAfter(current, instruction);
+                current = instruction;
+            }
+        }
+
+        private IEnumerable<Instruction> GetInstructions(MethodDefinition method)
+        {
+            yield return Instruction.Create(OpCodes.Nop);
+            yield return Instruction.Create(OpCodes.Ldstr, "Hello World.");
+            yield return Instruction.Create(OpCodes.Call, ModuleDefinition.ImportReference(_writeLineMethod));
+        }
+    }
+}
