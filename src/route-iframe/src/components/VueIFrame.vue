d001f7b3ce278a3abcf982d072aeee39447e617a
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-09-21 16:40:17 +0800

    添加新示例

---
+++ src/route-iframe/src/components/VueIFrame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <iframe id="vue-iframe" :src="url" frameborder="0" scrolling="auto" height="500px" width="1920px"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueIframe',
+  computed: {
+    url() {
+        return this.$route.query.url
+    }
+  }
+}
+</script>
+
