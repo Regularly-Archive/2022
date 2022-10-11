0e951cc5027eec21b617b9c8088a608087205c7f
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-10-11 12:18:55 +0800

    Add Large Files

---
+++ src/CSharpWasm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpWasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Greeter
+    {
+        public static string SayHello(string name)
+        {
+            return string.Format("Hello {0}", name);
+        }
+    }
+}
