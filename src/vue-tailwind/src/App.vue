8ecde6c5e4f55f0aaf9ea427354cb96b2684b292
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-10-25 17:13:02 +0800

    添加新示例

---
+++ src/vue-tailwind/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <hello-world/>
+    <toast-form title="你是认真的吗" content="你是猴子请来的逗比吗" confirmText="Yes" cancelText="No"/>
+  </div>
+</template>
+
+<script>
+// import MyHeader from './components/Header.vue'
+// import LoginForm from './components/LoginForm.vue'
+import ToastForm from './components/ToastForm.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // MyHeader,
+    // LoginForm,
+    ToastForm,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
