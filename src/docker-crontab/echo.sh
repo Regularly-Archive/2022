a4f9e728a4643caba43061791662a8c7727ac7b2
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-11-24 17:00:18 +0800

    提交新示例

---
+++ src/docker-crontab/echo.sh
@@ -0,0 +1,1 @@
+echo [$(date)] "Hello" > /proc/1/fd/1
---
+++ src/docker-crontab/entrypoint.sh
@@ -0,0 +1,8 @@
+# …˙≥… crontab ≈‰÷√
+cronfile='/usr/docker/cron.conf'
+echoJob='*/3 * * * * /usr/docker/echo.sh'
+echo "$echoJob" | tee -a $cronfile
+# º§ªÓ crontab
+crontab $cronfile
+/etc/init.d/cron restart
+apache2-foreground
