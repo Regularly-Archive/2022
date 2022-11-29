d916ca94ab32a9f0e1b79b2d2eb9e54fd6c6706b
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-11-29 18:16:52 +0800

    更新文档

--- src/NextCloud/custom_entrypoint.sh
+++ src/NextCloud/custom_entrypoint.sh
@@ -1,10 +1,13 @@
-﻿# 生成 crontab 配置
-cronfile='/usr/crontab/cron.conf'
+#!/bin/sh
+set -e
+
+echo 'initializing nextcloud and starting apache server...'
+/entrypoint.sh apache2-foreground
+
+echo "creating renew ticket cron job..."
+export cronfile='/usr/crontab/cron.conf'
 renewTicket='*/10 * * * * /usr/crontab/renew.sh'
 echo "$renewTicket" | tee -a $cronfile
-# 激活 crontab
 crontab $cronfile
 /etc/init.d/cron reload
 /etc/init.d/cron restart
-# 启动 Apache
-apache2-foreground
