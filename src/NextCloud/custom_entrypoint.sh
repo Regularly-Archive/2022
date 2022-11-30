ecac123b0c7d2928bac7640c66e97343b6adcc45
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-11-30 14:47:01 +0800

    完成重构

--- src/NextCloud/custom_entrypoint.sh
+++ src/NextCloud/custom_entrypoint.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 set -e
 
-echo 'initializing nextcloud and starting apache server...'
+#分配权限
+chown www-data:www-data /etc/apache2/krb-container.keytab
+chmod 777 /etc/apache2/krb-container.keytab
+chmod +x /usr/crontab/renew.sh
+
+#初始化票据
+echo 'Initializing Kerberos ticket...'
+kinit -kt /etc/apache2/krb-container.keytab HTTP/$NEXTCLOUD_SERVER_NAME@$DOMAIN_SERVER_NAME
+
+echo 'Initializing NextCloud and starting Apache server...'
 /entrypoint.sh apache2-foreground
 
-echo "creating renew ticket cron job..."
+#生成刷新票据的定时任务
+echo "Creating renew ticket cron job..."
 export cronfile='/usr/crontab/cron.conf'
 renewTicket='*/10 * * * * /usr/crontab/renew.sh'
 echo "$renewTicket" | tee -a $cronfile
 crontab $cronfile
 /etc/init.d/cron reload
 /etc/init.d/cron restart
+
+#如果作为 Docker 容器的入口脚本，建议在这里增加以下脚本来支持参数传递
+#其效果约等于 ENTRYPOINT CMD
+#exec "$@"
