396bdf9cb5cd77cf0a8e1c307fa908148e0bfbdc
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-12-12 12:34:20 +0800

    增加新示例

--- src/NextCloud/custom_entrypoint.sh
+++ src/NextCloud/custom_entrypoint.sh
@@ -7,20 +7,20 @@ chmod 777 /etc/apache2/krb-container.keytab
 chmod +x /usr/crontab/renew.sh
 
 #初始化票据
-echo 'Initializing Kerberos ticket...'
+echo [$(date)] 'Initializing Kerberos ticket...'
 kinit -kt /etc/apache2/krb-container.keytab HTTP/$NEXTCLOUD_SERVER_NAME@$DOMAIN_SERVER_NAME
 
-echo 'Initializing NextCloud and starting Apache server...'
-/entrypoint.sh apache2-foreground
-
 #生成刷新票据的定时任务
-echo "Creating renew ticket cron job..."
+echo [$(date)] "Creating renew ticket cron job..."
 export cronfile='/usr/crontab/cron.conf'
 renewTicket='*/10 * * * * /usr/crontab/renew.sh'
 echo "$renewTicket" | tee -a $cronfile
 crontab $cronfile
 /etc/init.d/cron reload
 /etc/init.d/cron restart
+
+echo [$(date)] 'Initializing NextCloud and starting Apache server...'
+/entrypoint.sh apache2-foreground
 
 #如果作为 Docker 容器的入口脚本，建议在这里增加以下脚本来支持参数传递
 #其效果约等于 ENTRYPOINT CMD
--- src/NextCloud/docker-reference/crontab/renew.sh
+++ src/NextCloud/docker-reference/crontab/renew.sh
@@ -1,2 +1,2 @@
-echo [$(date)] 'request a new ticket for HTTP/nextcloud.fgms.dev.com@FGMS.DEV.COM' > /proc/1/fd/1
-kinit -kt /etc/apache2/krb-container.keytab HTTP/nextcloud.fgms.dev.com@FGMS.DEV.COM
+echo [$(date)] 'request a new ticket for HTTP/'$NEXTCLOUD_SERVER_NAME'@'$DOMAIN_SERVER_NAME > /proc/1/fd/1
+kinit -kt /etc/apache2/krb-container.keytab HTTP/$NEXTCLOUD_SERVER_NAME@$DOMAIN_SERVER_NAME
